cdd82437aca8f5257b7807c26e72e7c9625b13aa
Seif Fouad <SeifAhmedFouad25@gmail.com>
2021-10-29 11:54:29 +0200

    First Commit

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 //Middlewares
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config();
 //Schemas
 const Flight = require('./Schemas/Flight')
 
@@ -8,16 +9,20 @@ const Flight = require('./Schemas/Flight')
 const addFlightRoute = require('./Routes/addFlight')
 //App
 const app = express()
-mongoose.connect('mongodb+srv://sampleUser:testpassword1001@flightdb.gqpep.mongodb.net/FlightDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoURI = process.env.MONGOURI
+const PORT = process.env.PORT
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() =>{
+        app.listen(PORT,()=> {
+        console.log(`listening on port https://localhost:${PORT}/`)
+        })
         console.log("MongoDB is now connected")
         
     })
     .catch(err => console.log(err));
 
-app.listen(3000,()=> {
-    console.log(`listening on port https://localhost:3000/`)
-})
+
 
 app.get('/',(req,res)=>{
     res.send('<h1>Hello</h1>')
