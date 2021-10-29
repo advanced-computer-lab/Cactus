c5b512fd84e4fb295cefc80942bd5c95d1e7a1a4
unknown <mazen.sadek@yahoo.com>
2021-10-29 13:43:32 -0700

    testtest2

--- Server/Routes/findFlight.js
+++ Server/Routes/findFlight.js
@@ -3,9 +3,36 @@ const findFlightRouter = express.Router()
 const mongoose = require('mongoose')
 const Flight = require('../Schemas/Flight')
 
+findFlightRouter.post('/',async (req,res) =>{
+  let {search}= req.body
+  console.log(search)
+//   Flight.find({
+//     "_id": search
+//     // "departureTime": ,
+//     // "arrivalTime":,
+//     // "departureDate": ,
+//     // "arrivalDate":,
+//     // "airport":
+// })
+//     .then((result)=>{
+//       res.send(result)
+//     })
+//     .catch((err)=>{
+//       console.log(err)
+//     })
+
+})
+
 
 findFlightRouter.get('/',(req,res)=>{ //TODO:
-    Flight.find()
+    Flight.find(
+      // "flightNumber": ,
+      // "departureTime": ,
+      // "arrivalTime":,
+      // "departureDate": ,
+      // "arrivalDate":,
+      // "airport":
+    )
       .then((result)=>{
         res.send(result)
       })
