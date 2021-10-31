f1452be98ae9434cc756308e0bbd96d2872bf9c9
unknown <mazen.sadek@yahoo.com>
2021-10-31 05:51:39 -0700

    delete donez probz

--- Server/Routes/findFlight.js
+++ Server/Routes/findFlight.js
@@ -2,37 +2,64 @@ const express = require('express')
 const findFlightRouter = express.Router()
 const mongoose = require('mongoose')
 const Flight = require('../Schemas/Flight')
+const bp = require('body-parser')
+findFlightRouter.use(bp.json())
+findFlightRouter.use(bp.urlencoded({extended: true}))
 
-findFlightRouter.post('/',async (req,res) =>{
-  let {search}= req.body
-  console.log(search)
-//   Flight.find({
-//     "_id": search
-//     // "departureTime": ,
-//     // "arrivalTime":,
-//     // "departureDate": ,
-//     // "arrivalDate":,
-//     // "airport":
-// })
-//     .then((result)=>{
-//       res.send(result)
-//     })
-//     .catch((err)=>{
-//       console.log(err)
-//     })
+findFlightRouter.post('/',(req,res) =>{
+  var f1=new Flight()
+
+
+  if(req.body._id){
+    var field = "_id"
+    f1[field]=req.body._id
+}
+  if(req.body.flightNumber){
+    field = "flightNumber"
+    f1[field]=req.body.flightNumber
+  }
+  if(req.body.departureTime){
+    field = "departureTime"
+    f1[field]=req.body.departureTime
+}
+  if(req.body.arrivalTime){
+    field = "arrivalTime"
+    f1[field]=req.body.arrivalTime
+}
+  if(req.body.departureDate){
+    field = "departureDate"
+    f1[field]=req.body.departureDate
+  }
+  if(req.body.arrivalDate){
+    field = "arrivalDate"
+    f1[field]=req.body.arrivalDate
+  }
+  if(req.body.airport){
+    field = "airport"
+    f1[field]=req.body.airport
+  }
+
+  Flight.find(f1)
+    .then((result)=>{
+      res.send(result)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
 
 })
 
 
 findFlightRouter.get('/',(req,res)=>{ //TODO:
-    Flight.find(
-      // "flightNumber": ,
+    Flight.find({
+      "_id": "check000"
+      //"flightNumber": ,
       // "departureTime": ,
       // "arrivalTime":,
       // "departureDate": ,
       // "arrivalDate":,
       // "airport":
-    )
+    })
       .then((result)=>{
         res.send(result)
       })
--- Server/index.js
+++ Server/index.js
@@ -8,6 +8,7 @@ const Flight = require('./Schemas/Flight')
 //Routers
 const addFlightRoute = require('./Routes/addFlight')
 const findFlightRoute = require('./Routes/findFlight')
+const deleteFlightRoute = require('./Routes/deleteFlight')
 
 //App
 const app = express()
@@ -33,3 +34,5 @@ app.get('/', (req, res) => {
 app.use('/addflight', addFlightRoute)
 
 app.use('/findFlight', findFlightRoute)
+
+app.use('/deleteFlight', deleteFlightRoute)
