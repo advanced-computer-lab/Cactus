a6c3166d66e6fd7b63475322dd906a7eeab7b978
Seif Fouad <SeifAhmedFouad25@gmail.com>
2021-11-02 23:14:46 +0200

    New Files

--- Server/Routes/Flight/Flight.js
+++ Server/Routes/Flight/Flight.js
@@ -1,11 +1,15 @@
+//___________Middleware___________
 const express = require('express')
 const FlightRouter = express.Router()
 const mongoose = require('mongoose')
-const Flight = require('../Schemas/Flight')
-const bp = require('body-parser')
-FlightRouter.use(bp.json())
-FlightRouter.use(bp.urlencoded({extended: true}))
 
+//___________Schema___________
+const Flight = require('../../Schemas/Flight')
+
+//___________Flight Router___________
+FlightRouter.use(express.json())
+
+// ________Add a New Flight________
 FlightRouter.post('/addFlight', (req, res) => {
     const flight = new Flight({ 
         'flightNumber': req.body.flightNumber,
@@ -26,82 +30,78 @@ FlightRouter.post('/addFlight', (req, res) => {
         })
 })
 
-
+// ________Find All Flights and Filter________
 FlightRouter.post('/findFlight',(req,res) =>{
     var f1=new Flight()
-  
-  
+
+
     if(req.body.flightNumber){
-      var field = "flightNumber"
-      f1[field]=req.body.flightNumber
+        var field = "flightNumber"
+        f1[field]=req.body.flightNumber
     }
     if(req.body.departureTime){
-      field = "departureTime"
-      f1[field]=req.body.departureTime
-  }
+        field = "departureTime"
+        f1[field]=req.body.departureTime
+    }
     if(req.body.arrivalTime){
-      field = "arrivalTime"
-      f1[field]=req.body.arrivalTime
-  }
+        field = "arrivalTime"
+        f1[field]=req.body.arrivalTime
+    }
     if(req.body.departureDate){
-      field = "departureDate"
-      f1[field]=req.body.departureDate
+        field = "departureDate"
+        f1[field]=req.body.departureDate
     }
     if(req.body.arrivalDate){
-      field = "arrivalDate"
-      f1[field]=req.body.arrivalDate
+        field = "arrivalDate"
+        f1[field]=req.body.arrivalDate
     }
     if(req.body.airport){
-      field = "airport"
-      f1[field]=req.body.airport
+        field = "airport"
+        f1[field]=req.body.airport
     }
-  
+
     Flight.find(f1)
-      .then((result)=>{
+        .then((result)=>{
         res.send(result)
         console.log(result)
-      })
-      .catch((err)=>{
-        console.log(err)
-      })
-  
-  })
-  
-  
-  FlightRouter.get('/findFlight',(req,res)=>{ //TODO:
-      Flight.find()
-        .then((result)=>{
-          res.send(result)
         })
         .catch((err)=>{
-          console.log(err)
+        console.log(err)
         })
-  })
 
-
-  FlightRouter.delete('/deleteFlight/:id',(req,res) =>{
-    Flight.findById(req.params.id)
-    .then(flight => flight.remove().then(() => res.json({success: true})))
-    .catch(er => res.status(404).json({success: false}))
+})
+  
+// ________Find All Flights________
+FlightRouter.get('/findFlight',(req,res)=>{
+    Flight.find()
+        .then((result)=>{
+            res.send(result)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
 })
 
-FlightRouter.put('/updateFlight/:id',(req,res) =>{
-
-
-
+// ________Delete a Flight________
+FlightRouter.delete('/deleteFlight/:id',(req,res) =>{
     Flight.findById(req.params.id)
-    .then(flight => 
-  
-  {
+        .then(flight => flight.remove().then(() => res.json({success: true})))
+        .catch(er => res.status(404).json({success: false}))
+})
+
+// ________Edit a Flight________
+FlightRouter.put('/updateFlight/:id',(req,res) =>{
+    Flight.findById(req.params.id)
+    .then(flight => {
     if(req.body.flightNumber){
       flight.flightNumber = req.body.flightNumber
     }
     if(req.body.departureTime){
         flight.departureTime = req.body.departureTime
-  }
+    }
     if(req.body.arrivalTime){
         flight.arrivalTime= req.body.arrivalTime
-  }
+    }
     if(req.body.departureDate){ 
         flight.departureDate= req.body.departureDate
     }
---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+//___________StyleSheets___________
+import './App.css';
+
+//___________Middleware___________
+import {BrowserRouter as Router, Switch, Route, Link}from 'react-router-dom';
+
+
+//___________Views___________
+import AdminHome from './Views/Admin/AdminHome'
+import UserHome from './Views/User/UserHome'
+import HomePage from './Views/HomePage'
+import AddFlight from './Views/Admin/Flight/AddFlight'
+import EditFlight from './Views/Admin/Flight/EditFlight'
+import FindFlight from './Views/Admin/Flight/FindFlight'
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/homepage">
+              <UserHome />
+            </Route>
+            <Route path="/adminHome">
+              <AdminHome />
+            </Route>
+            <Route path="/addFlight">
+              <AddFlight />
+            </Route>
+            <Route path="/editFlight">
+              <EditFlight />
+            </Route>
+            <Route path="/findFlight">
+              <FindFlight />
+            </Route>
+          </Switch>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
